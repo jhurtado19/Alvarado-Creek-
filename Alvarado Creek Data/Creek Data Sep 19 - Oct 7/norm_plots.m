f5502d909f7922ea555a993d1e3b5c5ab9c7e47b
WIRL-Lab <jhurtado5179@sdsu.edu>
2024-10-09 10:07:06 -0700

    updated norm plots script

--- Alvarado Creek Data/Creek Data Sep 19 - Oct 7/norm_plots.m
+++ Alvarado Creek Data/Creek Data Sep 19 - Oct 7/norm_plots.m
@@ -11,6 +11,7 @@ grid on;
 plot(t_vec,tRFU, 'Color','blue');
 plot(locs1, pks1, 'bo'); % 'ro' specifies red circles
 plot(locs2, pks2, 'go'); % 'go' specifies green circles
-legend('Temperature','tRFU')
+legend('Temperature','tRFU', 'mean peak time = 11:48 pm','mean peak time = 7:10 am')
 xlabel('September 19, 12:00 - Oct 7, 12:00')
 ylabel('Normalized, Z-Scored Data')
+title('Mean Periodicity of RFU = 24 hrs, Mean Periodicity of cTemp = 23.48 hrs')
